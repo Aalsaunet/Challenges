93a2e3d4afac34a997cb55f536b92dee3a224b04
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2023-04-04 00:01:32 +0200

    Submitted solution to the Boats to save people problem (https://leetcode.com/problems/boats-to-save-people/description/)

---
+++ BoatsToSavePeople/solution.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        // Assumption: people[i] always <= limit
+        
+        // 1. sort people into buckets by their weight
+        // Use dictionary with K = weight, V = count
+        unordered_map<int, int> peopleByWeight;
+        int remainingPeople = people.size();
+        int requiredBoats = 0;
+
+        for (auto p : people) {
+            if (peopleByWeight.count(p)) {
+                // Key exists 
+                peopleByWeight[p]++;
+            } else {
+                peopleByWeight[p] = 1; // alternatively peopleByWeight.insert(std::make_pair(p, 1));
+            }
+        }
+
+        // 2. Find combinations X == limit, then X == (limit - 1) etc
+        while (remainingPeople > 0) { 
+            int boatWeight = 0;
+            int peopleInBoat = 0;
+            requiredBoats++;
+            for (int i = limit; i > 0; i--) {
+                if (peopleByWeight.count(i) && peopleByWeight[i] > 0) {
+                    // Key exists 
+                    if (boatWeight + i <= limit) {
+                        boatWeight += i;
+                        peopleInBoat++;
+                        peopleByWeight[i++]--;
+                        remainingPeople--;             
+                    }
+                 
+                    if (boatWeight == limit || peopleInBoat == 2) {
+                       break; 
+                    }
+                } 
+            }
+        }
+
+        return requiredBoats;
+    }
+};
+
+int main() {
+    Solution solution;
+    std::vector<int> people;
+    people.push_back(3);
+    people.push_back(2);
+    people.push_back(2);
+    people.push_back(1);
+    int limit = 3;
+    
+    int result = solution.numRescueBoats(people, limit);
+    std::cout << "Number of required boats: " << result << endl;
+
+    return 0;
+}
