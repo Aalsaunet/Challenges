64a8c8eec723f551402eb5006733174a85e4a50b
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2025-12-02 19:37:36 +0100

    Fixed the solution from Day 1 so that both part 1 and part 2 are present in the same file

--- AdventOfCode2025/Day1_SecretEntrance/solution.go
+++ AdventOfCode2025/Day1_SecretEntrance/solution.go
@@ -12,7 +12,49 @@ const RoundTurnValue = 100
 var currentValue = 50
 var zeroCount = 0
 
-func main() {
+func part1() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		dir := line[0] // First character is the direction
+		if dir != 'L' && dir != 'R' {
+			fmt.Println("Invalid character in line:", dir)
+			return
+		}
+
+		dist, err := strconv.Atoi(line[1:]) // The rest of the string is the number -> parse to int
+		if err != nil {
+			fmt.Println("Invalid number in line:", line)
+			continue
+		}
+
+		diff := dist % RoundTurnValue
+		if dir == 'L' {
+			diff *= -1
+		} else {
+			diff *= 1
+		}
+
+		currentValue = (currentValue + diff) % RoundTurnValue
+		if currentValue == 0 {
+			zeroCount++
+		}
+	}
+	fmt.Println(zeroCount)
+}
+
+func part2() {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -63,4 +105,9 @@ func main() {
 		}
 	}
 	fmt.Println(zeroCount)
+}
+
+func main() {
+	part1()
+	part2()
 }
